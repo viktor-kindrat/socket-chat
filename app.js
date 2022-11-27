4e9f770a54a046ee24e6e07dd1b6fc7cf92f4bd2
Victor Kindrat <kindratvictor07@gmail.com>
2022-11-27 15:13:53 +0200

    Inital commit

---
+++ app.js
@@ -0,0 +1,30 @@
+let app = require('express')();
+let http = require('http').createServer(app);
+const PORT = 3000;
+let io = require('socket.io')(http);
+
+let userCount = 0;
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + "/index.html")
+})
+
+io.on('connection', function(socket) {
+    console.log('new user connect');
+    userCount++;
+    io.emit('getStatus', userCount)
+
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    })
+
+    socket.on('disconnect', function() {
+        console.log('user disconected');
+        userCount--;
+        io.emit('getStatus', userCount)
+    })
+})
+
+http.listen(PORT, function() {
+    console.log('server runnning on port ' + PORT)
+})
