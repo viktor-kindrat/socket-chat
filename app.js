a2e5b1b62f39dafb003051c955c27f19fb9b5d6b
Victor Kindrat <kindratvictor07@gmail.com>
2022-12-02 23:11:53 +0200

    upd

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-let app = require('express')();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_ALz391au8yXlHF3BZA8i7CgVY-XoWXc",
+    authDomain: "socketchat-6d26f.firebaseapp.com",
+    projectId: "socketchat-6d26f",
+    storageBucket: "socketchat-6d26f.appspot.com",
+    messagingSenderId: "770663460702",
+    appId: "1:770663460702:web:c8651958bc4709899ba9cf",
+    measurementId: "G-9XF3TJ18H6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+let nodeapp = require('express')();
 let http = require('http').createServer(app);
 const PORT = 3000;
 let io = require('socket.io')(http);
 
 let userCount = 0;
 
-app.get('/', function(request, response) {
+nodeapp.get('/', function(request, response) {
     response.sendFile(__dirname + "/index.html")
 })
 
